2b1886f9a8598b50d640bc799c1b68d03d513420
Hidgal Games <hidgal.games@gmail.com>
2021-04-23 02:41:59 +0700

    Добавлен класс локализируемой строки
    Добавлен класс строки, в которой хранится массив строк на различных языках
    Добавлен скрипт вывода и редактирования этой строки

---
+++ Assets/Scripts/Utility/TranslatableString.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum Languages
+{
+	EN,
+	RU
+}
+
+[System.Serializable]
+public class TranslatableString
+{
+	public List<string> strings = new List<string>();
+
+	public static implicit operator string(TranslatableString translatableString)
+	{
+		int languageId = PlayerPrefs.GetInt("language_id");
+        if (translatableString.strings[languageId] != null)
+        {
+			return translatableString.strings[languageId];
+		}
+		else if(translatableString.strings[(int)Languages.EN] != null)
+        {
+			return translatableString.strings[(int)Languages.EN];
+		}
+
+		translatableString.strings[(int)Languages.EN] = "";
+		return translatableString.strings[(int)Languages.EN];
+	}
+}
